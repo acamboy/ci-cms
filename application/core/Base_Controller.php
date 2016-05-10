2d00b9b04c862010ff3b8853c320af3a86a4a56b
acamboy <acamboy@gmail.com>
2016-05-10 16:06:06 +0700

    test HMVC $config['modules_locations'] = array(     FCPATH.'modules/' => '../../modules/', );
    Signed-off-by: acamboy <acamboy@gmail.com>

---
+++ application/core/Base_Controller.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Base_Controller extends MX_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+	
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Base_Controller.php */
+/* Location: ./application/core/Base_Controller.php */
